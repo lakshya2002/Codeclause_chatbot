a23a165f8cca7c987fdb6d73a7d844bdc6c32e28
lakshya <lakshya29.verma@gmail.com>
2023-07-27 16:31:12 +0530

    build complete

--- models/handling.py
+++ models/handling.py
@@ -9,37 +9,38 @@ import random
 from keras.models import load_model
 import tensorflow as tf 
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
+import tkinter
+from tkinter import *
+import sys
+import os
 
 # ======================================= Loading Data ======================================================================
-model = load_model('nlu/chatbot_model.h5')
+model = load_model("bot_model.h5")
 intents = json.loads(open('Data/intent.json','r',encoding='UTF-8').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
 # ============================================================================================================================
-def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+def pre_process(sentence):
+    nlp = spacy.load('en_core_web_sm')
+    # Tokenize the sentence and create a list of lemmatized words
+    sentence_words = [token.lemma_ for token in nlp(sentence)]
     return sentence_words
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
+    # Tokenize the pattern
+    sentence_words = pre_process(sentence)
+    # Bag of words - matrix of N words, vocabulary matrix
+    bag = [0] * len(words)  
     for s in sentence_words:
-        for i,w in enumerate(words):
+        for i, w in enumerate(words):
             if w == s: 
-                # assign 1 if current word is in the vocabulary position
+                # Assign 1 if the current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("Found in bag: %s" % w)
+    return np.array(bag)
+
 
 def predict_class(sentence, model):
     # filter out predictions below a threshold
@@ -62,6 +63,7 @@ def getResponse(ints, intents_json):
             result = random.choice(i['responses'])
             break
     return result
+
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
@@ -99,5 +101,66 @@ def start_chat():
              print(f"Code Clause: {chatbot_response(inp)}"+'\n')
              print("-"*50)
 
-start_chat()
-print('all ok')
+# start_chat()
+
+
+import os
+from tkinter import Tk, Text, Button, Scrollbar, DISABLED, END, FALSE
+
+if os.environ.get('DISPLAY', '') == '':
+    print('no DISPLAY found. Using: 0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
+base = Tk()
+base.title("Chatbot")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+# Dark Theme
+base.config(bg="#121212")
+
+# Create Chat window
+ChatLog = Text(base, bd=0, bg="#121812", fg="#f0f0f0", height="8", width="50", font="Arial")
+ChatLog.config(state=DISABLED)
+
+# Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+# Create Button to send message
+SendButton = Button(base, font=("Verdana", 10, 'bold'), text="Send", width="10", height=1,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff')
+
+# Create the box to enter message
+EntryBox = Text(base, bd=0, bg="#1c1c1c", fg="#f0f0f0", width="29", height="5", font="Arial")
+
+# Function to send the message
+def send():
+    msg = EntryBox.get("1.0", 'end-1c').strip()
+    EntryBox.delete("0.0", END)
+
+    if msg != '':
+        ChatLog.config(state='normal')
+        ChatLog.tag_config("user", justify='right')  # Align user response to the right
+        ChatLog.tag_config("bot", justify='left')  # Align bot response to the left
+
+        ChatLog.insert(END, "You: " + msg + '\n\n', "user")
+        res = chatbot_response(msg)  # Call the chatbot_response function here to get the chatbot's response
+        ChatLog.insert(END, "Bot: " + res + '\n\n', "bot")
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+# Bind Enter key to send message
+def on_enter(event):
+    send()
+
+EntryBox.bind("<Return>", on_enter)
+
+# Place all components on the screen
+scrollbar.place(x=376, y=6, height=386)
+ChatLog.place(x=6, y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+base.mainloop()
